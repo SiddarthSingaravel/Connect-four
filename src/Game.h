845c8cb749e9019b0628cbc9f01031e0e6563f61
Siddarth Singaravel <sidd.singaravel@gmail.com>
2021-03-18 12:24:46 +0530

    Header File

---
+++ src/Game.h
@@ -0,0 +1,35 @@
+#ifndef __GAME_H_INCLUDED__
+#define __GAME_H_INCLUDED__
+
+#include "Board.h"
+
+class Game
+{
+    int currentPlayer;
+    Board board;
+    int winner;
+    bool gameOver;
+
+public:
+    // init board object, player
+    Game();
+    // game loop
+    void loop();
+    void start();
+    int findBestMove();
+
+private:
+    void togglePlayer();
+    /**
+     * @param depth Current lookahead depth
+     * @param maxDepth max lookahead depth
+     * @param isMaximizingPlayer max player or not
+     * @param player player symbol
+     * @return player symbol if win else 0
+    */
+    int minimax(int depth, int maxDepth, bool isMaximizingPlayer, int player);
+    int negamax(int depth, int maxDepth, bool isMaximizingPlayer, int player);
+    int alphabetapruning(int depth, int maxDepth, int alpha, int beta, bool isMaximizingPlayer, int player);
+};
+
+#endif
